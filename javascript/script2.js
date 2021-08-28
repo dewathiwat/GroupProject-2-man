a794870b7a04a3e711f0f0653489f56fd72fb7fb
ThanakornJ <thanakorn_jansa@elearning.cmu.ac.th>
2021-08-28 22:07:24 +0700

    html css

--- javascript/script2.js
+++ javascript/script2.js
@@ -1,12 +1,13 @@
 // !page 2
 let searchParams = new URLSearchParams(window.location.search).get("id");
 var re = "";
+
+
+
 const getProductOneDetail = async () => {
   console.log(searchParams);
   try {
-    const response = await axios.get(
-      "https://6102d7aa79ed680017482359.mockapi.io/productdetail"
-    );
+    const response = await axios.get("https://6102d7aa79ed680017482359.mockapi.io/productdetail");
     let resulte = response.data.filter((data) => {
       return data.id == searchParams;
     });
@@ -20,67 +21,66 @@ const getProductOneDetail = async () => {
     console.log(e.message);
   }
 };
+
 getProductOneDetail();
 let text = "";
-async function ShowDetail(data,searchParams) {
+
+function ShowDetail(data,searchParams) {
   console.log("show", data);
 
   data.prdSize.forEach(myFunction);
 
   document.getElementById("Detail").innerHTML = `
-      
-
 <div class="card col-lg-7 col-xs-12">
 <img class="pic_select card-img-top" src="${data.prdImageUrl}" alt="Card image cap">
 </div>
 </div>
 <div class=" col-lg-4 col-xs-12">
-
 <h2>Product name: ${data.prdname}</h2>
 <h4 class="text-danger">${data.prdPrice} THB</h4>
 <p class="text-secondary">Size</p>
 <div><select name="size" size="1">
 <option value="default" selected>
 Please Select
-
 </option>
 ${text}
-
 </select> </div>
 <br>
-<button class="btn btn-dark text-white" onclick="${await setlocal(re)} ">Add To Bag</button>
+<button class="btn btn-dark text-white"  id="demo" >Add To Bag</button>
 <br>
 <br>
-
-<h5>Product Details: ${data.txtDetail}</h5>
-
-<button class="btn btn-secondary  text-dark"onclick="">show detail</button>
+<h3>Product Details</h3>
+<h5>${data.txtDetail}</h5>
+<button class="btndetail btn btn-secondary  text-dark" id="myBtn"> show detail</button>
 </div>
 </div>
 </div>
-
-
         `
-  
 }
+
+
+
+
+
+
 
 function myFunction(item) {
   text += `<option value="${item}" >
   ${item}
-  
   </option> `
 }
-function setlocal(data) {
-  const oldProductCart = JSON.parse(localStorage.getItem("dataproduct"))
+
+async function setlocal(data) {
+  const oldProductCart = await JSON.parse(localStorage.getItem("dataproduct"))
   const newProductCart = data
   if(oldProductCart==null){
   let card=newProductCart;
   localStorage.setItem("dataproduct", JSON.stringify(card))
-  // alert('addproduct "Success1"')
+  alert('addproduct "Success1"')
   }else{
   let card=oldProductCart.concat(newProductCart)
   localStorage.setItem("dataproduct", JSON.stringify(card))
-  // alert('addproduct "Success2"')
+  alert('addproduct "Success2"')
 }
 
 
--- javascript/script3.js
+++ javascript/script3.js
@@ -1,86 +1,201 @@
 // !page 3
 
-// let searchParams = new URLSearchParams(window.location.search).get("id");
-
-// const getProductBag = async () => {
-//     try {
-//       const response = await axios.get(
-//         "https://6102d7aa79ed680017482359.mockapi.io/"
-//       );
-//       document.getElementById("test1").innerHTML = response.data
-//         .map(
-//           (product) =>
-//    `
-//   <div class="row">
-//   <div class="col-8">
-//   <div class="col-4">
-//   <img src="${product.prdImageUrl}"> </div>
-//    </div>
-//   <div class="col-4">
-//    <h4 class="text-danger">${product.prdPrice} THB</h4>
-//    <h2>Product name: ${product.prdname}</h2>
-//    </div>
-  
-//   </div>
-  
-//   <div class="col-4">
-  
-//   <div class="row">
-//   <h3>bag</h3><h3>bag</h3><h3>bag</h3><h3>bag</h3><h3>bag</h3>
-//   </div>
-//   </div>
-//   </div>
-//           `
-//         )
-//         .join("");
-//     } catch (e) {
-//       console.log(e.message);
-//     }
-//   };
-//   getProductBag();
- output =document.getElementById('output')
+output = document.getElementById("output");
 const dataJson = localStorage.getItem("dataproduct");
-    let obj =JSON.parse(dataJson)
-    console.log(obj);
-    count(obj)
+let obj = JSON.parse(dataJson);
+console.log(obj);
+count(obj);
+
+function count(myObj) {
+  for (let x in myObj) {
+    addtext(myObj[x]);
+  }
+  addpay()
+}
+function addpay() {
+
+//! กล่องที่ 2
+let divPay = document.createElement("div"); //กล่องที่ 2
+divPay.classList.add("col-2");
+let divPayContent =document.createElement("div");
+divPayContent.classList.add("card");
+let ulPay =document.createElement("ul");
+ulPay.classList.add("list-group");
+
+ulPay.classList.add("list-group-flush");
+
+let liPay =document.createElement("il");
+liPay.classList.add("list-group-item");
+let liPay2 =document.createElement("il");
+liPay2.classList.add("list-group-item");
+let liPay3 =document.createElement("il");
+liPay3.classList.add("list-group-item");
+let h3 =document.createElement("h3");
+let p1 =document.createElement("p");
+let p2 =document.createElement("p");
+let buttonPay =document.createElement("button");
+buttonPay.setAttribute("type","button")
+buttonPay.classList.add("btn");
+buttonPay.classList.add("btn-secondary");
+buttonPay.classList.add("btn-block");
+buttonPay.textContent="Checkout"
+
+
+
+liPay.appendChild(h3);
+h3.textContent="sunmary"
+liPay2.appendChild(p1);
+p1.textContent="Subtotal"
+liPay3.appendChild(p2);
+p2.textContent="total"
+
+let contentPay = document.querySelector(".pay");
+contentPay.appendChild(divPayContent)
+divPayContent.appendChild(ulPay)
+
+
+ulPay.appendChild(liPay)
+ulPay.appendChild(liPay2)
+ulPay.appendChild(liPay3)
+ulPay.appendChild(buttonPay)
+
+
 
     
-      
-    
-function count (myObj){
-    
-    for (let x in myObj) {
+}
 
-      addtext(myObj[x])
-    }
-    
-    }
+// function myFunction(data) {
+//   text += `<option value="${data}" >
+//   ${item}
+//   </option> `
+// }
 
-    function addtext(data) {
-        let Allmight = document.createElement('div')
-        Allmight.classList.add('row')
-        let one = document.createElement('div')
-        one.classList.add('col-4')
-        let two = document.createElement('div')
-        two.classList.add('col-8')
-        let img = document.createElement('img')
-        img.classList.add("card-img")
-        let imgname = data.prdImageUrl
-        img.setAttribute('src', imgname)
-    
-        let inone = document.createElement('div')
-        inone.classList.add("card-body")
-        let H5 = document.createElement('h5')
-        H5.classList.add("card-title")
-        let name = data.prdname
-        H5.innerHTML = name
-        inone.appendChild(H5)   
-        one.appendChild(img)
-        two.appendChild(inone)
-        Allmight.appendChild(one)
-        Allmight.appendChild(two)
-        output.appendChild(Allmight)
-    }
-    
-   
+
+
+
+function addtext(data) {
+
+
+  let section = document.querySelector("aside"); // กล่องด้านนอก
+  section.classList.add('row')
+  let divAllContent=document.createElement("div");
+  divAllContent.classList.add('row')
+
+
+  let divAll = document.createElement("div"); // กล่องที่ 1
+  divAll.classList.add("col-10");
+  divAll.classList.add("mb-5");
   
+  let divContent = document.createElement("div"); //กล่องที่ 1
+  divContent.classList.add("col-10"); //กล่องที่ 1
+  let divContentRow = document.createElement("div"); //ของที่อยู่ในกล่องที่ 1
+  divContentRow.classList.add("row"); //กล่องย่อยที่อยู่ในกล่องที่ 1
+  let divPicCol = document.createElement("div"); //รุปที่อยู่ในกล่องที่1
+  divPicCol.classList.add("col-4"); //รุปที่อยู่ในกล่องที่1
+  let divContentCol =document.createElement("div");//ข้อความที่อยู่ในกล่องที่1
+  divContentCol.classList.add("col-8"); //ข้อความที่อยู่ในกล่องที่1
+  
+// เข้าไปอีกชั้นของกล่องที่ 1
+//! กล่องที่ 1
+let divselectqtyandsize =  document.createElement("div");
+divselectqtyandsize.classList.add("row");
+let divselectSize =document.createElement("div");
+divselectSize.classList.add("col-6");
+let divselectQty=document.createElement("div");
+divselectQty.classList.add("col-6");
+
+ let pSize = document.createElement("p");
+  pSize.textContent = "Size";
+
+ let pQty = document.createElement("p");
+  pQty.textContent = "Qty";
+
+
+
+// ตัวเลือก Size
+  let sizeDisplay = document.createElement("select");
+//   let prdSize = data.prdSize;
+let sizeOption = document.createElement("option");
+sizeOption.setAttribute("value","defult")
+  sizeDisplay.setAttribute("name","size");
+  sizeOption.textContent='PLESE SELECTED';
+//  ตัวเลือก QTY
+
+let qtyDisplay = document.createElement("select");
+//   let prdSize = data.prdSize;
+let qtyOption = document.createElement("option");
+qtyOption.setAttribute("value","defult")
+qtyDisplay.setAttribute("name","size");
+qtyOption.textContent='PLESE SELECTED';
+let qtyOptionsize =  document.createElement("option");
+let prdSize = data.prdSize;
+qtyOptionsize.setAttribute("value",prdSize)
+
+
+
+  let pRemove = document.createElement("a");
+  pRemove.setAttribute("href","page1.html")
+  pRemove.textContent = "Remove this item";
+  pRemove.style.color="red";
+
+ 
+  let selectSizeDiv = document.createElement("div");
+  selectSizeDiv.classList.add("col-6");
+ 
+  let selectQty = document.createElement("div");
+  selectQty.classList.add("col-6");
+ 
+
+// รูปภาพ
+  let pic = document.createElement("img");
+  pic.classList.add('card-img-top');
+  let imgname = data.prdImageUrl;
+  pic.setAttribute("src", imgname);
+
+// เนื้อหา
+let headName = document.createElement("h3");
+  let priceName = document.createElement("h2");
+  let prdname = data.prdname;
+  let prdPrice = data.prdPrice;
+  priceName.textContent = prdPrice + " THB";
+  priceName.style.color = "red";
+  headName.textContent = prdname;
+
+
+
+
+
+
+
+// จัดของใส่กล่อง  
+// let section = document.querySelector("aside");
+let contentPrd = document.querySelector(".contentPrd");
+
+contentPrd.appendChild(divPicCol)
+divPicCol.appendChild(pic)
+contentPrd.appendChild(divContentCol)
+divContentCol.appendChild(headName)
+divContentCol.appendChild(priceName)
+divContentCol.appendChild(divselectqtyandsize)
+divselectqtyandsize.appendChild(divselectSize)
+divselectSize.appendChild(pSize)
+
+
+
+divselectSize.appendChild(sizeDisplay)
+sizeDisplay.appendChild(sizeOption)
+// sizeOption.appendChild(defultOption)
+divselectqtyandsize.appendChild(divselectQty)
+divselectQty.appendChild(pQty)
+
+divselectQty.appendChild(qtyDisplay)
+qtyDisplay.appendChild(qtyOption)
+qtyDisplay.appendChild(qtyOptionsize)
+
+
+
+divContentCol.appendChild(pRemove)
+
+
+
+}
