1498c10b82f8fa12f6c16bff0c0c968952049f69
dewathiwat <Athiwat_Jansomwong@cmu.ac.th>
2021-08-30 00:41:07 +0700

    ใกล้เสร็จละ

--- javascript/script3.js
+++ javascript/script3.js
@@ -3,13 +3,14 @@
 output = document.getElementById("output");
 const dataJson = localStorage.getItem("dataproduct");
 let obj = JSON.parse(dataJson);
+var totalprice = 0
 console.log(obj);
 count(obj);
 
 function count(myObj) {
   for (let x in myObj) {
     if (myObj[x]) {
-      addtext(myObj[x], x);
+    addtext(myObj[x], x);
     }
   }
   addpay();
@@ -46,7 +47,7 @@ function addpay() {
   liPay2.appendChild(p1);
   p1.textContent = "Subtotal";
   liPay3.appendChild(p2);
-  p2.textContent = "total";
+  p2.textContent = `total ${totalprice}`;
 
   let contentPay = document.querySelector(".pay");
   contentPay.appendChild(divPayContent);
@@ -58,13 +59,10 @@ function addpay() {
   ulPay.appendChild(buttonPay);
 }
 
-// function myFunction(data) {
-//   text += `<option value="${data}" >
-//   ${item}
-//   </option> `
-// }
+
 
 async function addtext(data, x) {
+  let qty=1
   let section = document.querySelector("aside"); // กล่องด้านนอก
   section.classList.add("row");
   let divAllContent = document.createElement("div");
@@ -118,7 +116,7 @@ async function addtext(data, x) {
   qtyOptionsize.setAttribute("value", prdSize);
 
   let pRemove = document.createElement("a");
-  // pRemove.setAttribute("href","page1.html")
+   pRemove.setAttribute("href","page3.html")
   pRemove.textContent = "Remove this item";
   pRemove.style.color = "red";
   pRemove.addEventListener("click", () => {
@@ -144,10 +142,11 @@ async function addtext(data, x) {
   let headName = document.createElement("h3");
   let priceName = document.createElement("h2");
   let prdname = data.prdname;
-  let prdPrice = data.prdPrice;
+  let prdPrice = parseInt(data.prdPrice);
   priceName.textContent = prdPrice + " THB";
   priceName.style.color = "red";
   headName.textContent = prdname;
+  totalprice += prdPrice
 
   // จัดของใส่กล่อง
   // let section = document.querySelector("aside");
@@ -173,4 +172,5 @@ async function addtext(data, x) {
   qtyDisplay.appendChild(qtyOptionsize);
 
   divContentCol.appendChild(pRemove);
+  return (qty*prdPrice);
 }
